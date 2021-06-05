1181b25483d4c65ff5889a04a7f3fd88c276952a
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-05 13:17:19 +0900

    Add get_at function

--- list/src/list/simplyLinkedList.java
+++ list/src/list/simplyLinkedList.java
@@ -20,4 +20,16 @@ public class simplyLinkedList {
 		}
 		tail = newNode;
 	}
+	
+	public node get_at(int index) {
+		if (header == null) {
+			return null;
+		} else {
+			node nextNode = header;
+			for (int i = 0; i < index; i++) {
+				nextNode = header.get_next();
+			}
+			return nextNode;
+		}
+	}
 }
