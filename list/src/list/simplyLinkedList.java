7d2931a350677275a1649460396a44fd78415250
ChoEChoU <44629655+ChoEChoU@users.noreply.github.com>
2021-06-05 13:01:28 +0900

    Write simplyLinkedList code

--- list/src/list/simplyLinkedList.java
+++ list/src/list/simplyLinkedList.java
@@ -1,10 +1,25 @@
 package list;
 
 public class simplyLinkedList {
-	private node header;
-	private int size;
+	private node header = null;
+	private int size = 0;
 	
 	public simplyLinkedList() {
 		
+	}
+	
+	public void append(String fruit) {
+		node newNode = new node(fruit);
+		size++;
+		if (header == null) {
+			header = newNode;
+		}
+		else {
+			node tempNode = header;
+			while (tempNode.get_next() != null) {
+				tempNode = tempNode.get_next();
+			}
+			tempNode.set_next(tempNode);
+		}
 	}
 }
